21ab315990f047ddb86aa42114d63c3f5f866f2b
Ben Clive <bac2g10@ecs.soton.ac.uk>
2011-06-15 15:58:33 +0100

    Added tick support

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ class Tetroid:
 	def __init__(self):
 		self.type = random.randint(0,4)
 		self.colour = random.randint(0,4)
-		self.squares = [(8,8), (8,9), (9,9), (9,8)]
+		self.squares = [[8,8], [8,9], [9,9], [9,8]]
 class Game:
 	def __init__(self):
 		self.tetroids = []
@@ -12,7 +12,6 @@ class Game:
 		self.gridWidth = 10
 		self.currentTetroid = Tetroid()
 		self.tetroids.append(self.currentTetroid)
-		self.run()
 
 	def left():
 		skip
@@ -23,15 +22,30 @@ class Game:
 
         def tick(self):
 		#drop current tetroid 1 space
-		i = 0
+		mins = []
 		for x in self.currentTetroid.squares:
-			while(i < len(self.currentTetroid.squares)-1):
-				print x[1]
-				i += 1
+			for y in self.currentTetroid.squares:
+				curMin = y
+				if(x[1] == y[1]): #y values are the same
+					if(x[0]<y[0]):
+						curMin = x
+			if( curMin not in mins ):
+				mins.append(curMin)
+		print mins
+		#mins holds minimum items, check below them.
+		for coord in mins:
+			for x in self.tetroids:
+				for y in x.squares:
+					if(coord[0] == y[0] and coord[1]-1 == y[1] ):
+						
+						print coord[0]-1, y[0]
+
+		#Nothing below then move down
+
+		for x in self.currentTetroid.squares:
+			x[1] = x[1]-1
+
+		print self.currentTetroid.squares
 
         def run(self):
-		while True:
-			self.tick()
-			
-			
-
+		self.tick()
