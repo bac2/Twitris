196eddbb4446759c0c989d93ebc0bc59e9a30af6
Ben Clive <bac2g10@ecs.soton.ac.uk>
2011-08-17 22:52:31 +0000

    Added server support and OO controllers

--- control.py
+++ control.py
@@ -1,6 +1,7 @@
 from game import *
 from server import *
-import random
+import random,copy
+
 class Controller:
 
     def visualise(self):
@@ -11,24 +12,38 @@ class Controller:
             for y in range (0, 10):
                 self.arr[x].append(0)
 
-        for x in tetris.tetroids:
+        for x in self.game.tetroids:
            for y in x.squares:
 		print y[0], y[1]
                 self.arr[y[1]][y[0]] = 1
-	for y in tetris.currentTetroid.squares:
+	for y in self.game.currentTetroid.squares:
 	    self.arr[y[1]][y[0]] = 1
 	self.arr.reverse()
 	for x in self.arr:
 	    print x
 	print 
-c = Controller()
-tetris = Game()
-c.visualise()
-s = Server("127.0.0.1", 6500)
 
-while (True):
-    c.visualise()
-    tetris.tick()
-    tetris.rotate()
-    s.sendEvent(tetris.tetroids)
-    time.sleep(1)
+    def sendEvent(self):
+      data = copy.copy(self.game.tetroids)
+      data.append(self.game.currentTetroid)
+      d = simplejson.JSONEncoder(default=self.s.tetroidEncode).encode(data)
+      for client in self.s.clients:
+        client.send(d)
+
+    def __init__(self):
+      self.game = Game()
+      self.visualise()
+      self.s = Server("127.0.0.1", 6500, self.game)
+      self.s.start()
+
+      while 1:
+        self.tick()
+    
+    def tick(self):
+      self.visualise()
+      self.game.tick()
+      self.game.rotate()
+      self.sendEvent()
+      time.sleep(1)
+
+c = Controller()
--- server.py
+++ server.py
@@ -1,17 +1,52 @@
 import socket
+import simplejson
+from game import Tetroid
+import copy, threading
 
-class Server:
+class Server(threading.Thread):
 
-  def __init__(self, host, port):
+  def __init__(self, host, port, game):
+
+    threading.Thread.__init__(self)
 
     self.clients = []
+    self.game = game
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.sock.bind((host, port))
     self.sock.listen(5)
-    self.conn, self.addr = self.sock.accept()
-    self.clients.append((self.conn,self.addr))
-    
-  def sendEvent(self, data):
-    for x in self.clients:
-      x[0].send(data)
+
+
+  def run(self):
+    running = 1
+    while running:
+      connection = self.sock.accept()
+      self.accept(connection[0], connection[1])
+      
+
+  def accept(self, conn, addr):
+    #Send out the info of the current board, the size etc.
+    c = Client((conn,addr))
+    c.start()
+    self.clients.append(c)
+    d = simplejson.JSONEncoder().encode([{"gridWidth":self.game.gridWidth},{"gridHeight":self.game.gridHeight},{"lines":self.game.lines}])
+    conn.send(d)
+    d = copy.copy(self.game.tetroids)
+    d.append(self.game.currentTetroid)
+    conn.send(simplejson.JSONEncoder(default=self.tetroidEncode).encode(d))
+    print "Connection initated from ", addr
+
+
+  def tetroidEncode(self,obj):
+    if isinstance(obj, Tetroid):
+      return {"type":obj.type, "colour":obj.colour, "squares":obj.squares}
+    raise TypeError(repr(o) + " is not JSON serializable.")
+
+class Client(threading.Thread):
+  def __init__(self, (conn, addr)):
+    threading.Thread.__init__(self)
+    self.conn = conn
+    self.addr = addr
+
+  def send(self,data):
+    self.conn.send(data)
 
