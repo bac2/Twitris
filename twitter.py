d22e91aa26868709de60b3fb9872d9343e2fcfaf
Ben <ben@ubuntu.(none)>
2012-10-18 09:53:52 -0700

    Added twitter class for the Streaming API

--- twitter.py
+++ twitter.py
@@ -1,6 +1,25 @@
-import httplib
+import pycurl, urllib
 
-conn = httplib.HTTPConnection("www.google.com")
-conn.request("GET", "/index.html")
-r1 = conn.getresponse()
-print r1.read()
+USER = "benaclive"
+PASS = ""
+
+class Twitter:
+	def __init__(self):
+		self.buffer = ""
+		self.conn = pycurl.Curl()
+		self.conn.setopt(pycurl.USERPWD, "%s:%s" % (USER, PASS))
+		self.conn.setopt(pycurl.URL, "https://stream.twitter.com/1.1/statuses/filter.json")
+		self.conn.setopt(pycurl.WRITEFUNCTION, self.on_data)
+		self.conn.setopt(pycurl.POSTFIELDS, urllib.urlencode( {"track": "raining"}) )
+		self.conn.perform()
+
+	def on_data(self, data):
+		print data
+		self.buffer += data
+		if data.endswith("rn") and self.buffer.strip():
+			content = json.loads(self.buffer);
+			self.buffer = ""
+
+			print content
+
+twit = Twitter()
